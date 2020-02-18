f88a3b739c14841f32f2513291196397d243f5e7
JackAnati <Anatijack0@gmail.com>
2020-02-18 11:40:47 +0200

    first commit

---
+++ src/Execirse1.java
@@ -0,0 +1,15 @@
+public class Execirse1 {
+
+    public static void main(String args[]){
+        int a[]=new int[5];   //declaration and instantiation
+        a[0]=5;  //initialization
+        a[1]=10;
+        a[2]=15;
+        a[3]=20;
+        a[4]=50;
+
+//traversing array
+        for(int i=0;i<a.length;i++)   //length is the property of array
+            System.out.println(a[i]);
+    }
+}
